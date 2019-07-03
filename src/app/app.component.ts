f0e5f3a81c3f7121e89fccb7a6086b1d9e38d8b9
TinyKitten <catwalk8205@gmail.com>
2019-07-03 19:35:20 +0900

    アップデート確認実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'trainlcd';
+export class AppComponent implements OnInit {
+  constructor(private updates: SwUpdate) {}
+
+  ngOnInit() {
+    this.updates.available.subscribe(() => {
+      const prompt = window.confirm(
+        'アップデートがあります。今すぐ更新しますか？'
+      );
+      if (prompt) {
+        this.updates.activateUpdate().then(() => document.location.reload());
+      }
+    });
+  }
 }
